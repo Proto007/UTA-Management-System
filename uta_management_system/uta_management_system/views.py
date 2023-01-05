fba200dce4df3d87ff71e823c13a8e7300f580cc
Sadab Hafiz <sadabhafizny@gmail.com>
2023-01-05 18:31:15 -0500

    Revert "using random endpoints instead of random code"
    This reverts commit 1b1261de1d03dbc3f9bb83dab337ae3261a0b429.

--- uta_management_system/uta_management_system/views.py
+++ uta_management_system/uta_management_system/views.py
@@ -5,17 +5,6 @@ def homepage(request):
     """
     Render homepage.html in default endpoint
     """
-    return render(request,'homepage.html')
-
-def getpass(request):
     response = requests.post("http://127.0.0.1:8000/api/6d975d9e9f5e6d5a461ded16097ec288/", json={})
     data = response.json()
-    return render(request,'getpass.html', {'data':data})
-
-def checkin(request, random_pass):
-    response = requests.post("http://127.0.0.1:8000/api/6d975d9e9f5e6d5a461ded16097ec288/", json={})
-    data = response.json()
-    new_pass = data['random_pass']
-    if random_pass == new_pass:
-        return render(request,'checkin.html')
-    return render(request, 'homepage.html')
+    return render(request,'homepage.html', {'data':data})
