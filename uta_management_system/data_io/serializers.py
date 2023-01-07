cdcc10b56b3253f9909c2e873a8adf8bb03c8c97
Sadab Hafiz <sadabhafizny@gmail.com>
2023-01-06 23:41:34 -0500

    apply `isort` and `black` formatting

--- uta_management_system/data_io/serializers.py
+++ uta_management_system/data_io/serializers.py
@@ -1,35 +1,39 @@
 from rest_framework import serializers
+
 from .models import *
+
 
 class DataIOSerializer(serializers.ModelSerializer):
     class Meta:
         model = DataIO
-        fields = ('file_link',)
-        lookup_field = 'file_link'
+        fields = ("file_link",)
+        lookup_field = "file_link"
+
 
 class ShiftSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shift
-        fields = ('description', 'day', 'start', 'end')
-        lookup_field = 'description'
+        fields = ("description", "day", "start", "end")
+        lookup_field = "description"
+
 
 class UTASerializer(serializers.ModelSerializer):
     class Meta:
         model = UTA
-        fields = ('lastname', 'firstname', 'emplid', 'shifts' )
-        lookup_field = 'emplid'
+        fields = ("lastname", "firstname", "emplid", "shifts")
+        lookup_field = "emplid"
+
 
 class RandomPassSerializer(serializers.ModelSerializer):
     class Meta:
         model = RandomPass
-        fields = ['random_pass']
-        read_only_fields = ['random_pass']
+        fields = ["random_pass"]
+        read_only_fields = ["random_pass"]
+
 
 class CheckinSerializer(serializers.ModelSerializer):
     class Meta:
         model = Checkin
-        fields = ['emplid', 'shift', 'late_mins', 'number_of_shifts', 'alternate_day']
-        read_only_fields = ['shift', 'late_mins']
-        extra_kwargs = {
-            'alternate_day': {'write_only': True}
-        }
+        fields = ["emplid", "shift", "late_mins", "number_of_shifts", "alternate_day"]
+        read_only_fields = ["shift", "late_mins"]
+        extra_kwargs = {"alternate_day": {"write_only": True}}
--- uta_management_system/data_io/urls.py
+++ uta_management_system/data_io/urls.py
@@ -1,15 +1,18 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from .views import *
 
 app_name = "dataio"
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'update_schedule',DataIOViewSet,basename="update_schedule")
-router.register(r'6d975d9e9f5e6d5a461ded16097ec288',RandomPassViewSet,basename="random_pass")
-router.register(r'checkin', CheckinViewSet, basename="checkin")
+router.register(r"update_schedule", DataIOViewSet, basename="update_schedule")
+router.register(
+    r"6d975d9e9f5e6d5a461ded16097ec288", RandomPassViewSet, basename="random_pass"
+)
+router.register(r"checkin", CheckinViewSet, basename="checkin")
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
